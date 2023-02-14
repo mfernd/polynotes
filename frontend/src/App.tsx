b4d2c94ac4a59481bc591d72ff71803803f5661e
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-02-14 17:33:43 +0100

    Update App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
+    <>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
