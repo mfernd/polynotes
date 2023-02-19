edee3f611b9eb8cb5632ea1e82c8c6c3a03ab061
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-02-19 19:39:00 +0100

    refactor: button more customizable

--- frontend/src/components/Button.tsx
+++ frontend/src/components/Button.tsx
@@ -7,36 +7,44 @@ type ButtonProps = {
 };
 
 export const Button = ({ children = 'NONE', onClick }: ButtonProps) => {
+  const btn = buttonCss('#fff', '#fe0096', '#e40087',5, 3);
+
   return (
-    <button onClick={onClick} css={buttonCss(5, 2)}>
+    <button onClick={onClick} css={btn}>
       {children}
     </button>
   );
 };
 
-const buttonCss = (initHeight: number, addHeight: number) => css`
+const buttonCss = (
+  color: string,
+  bgColor: string,
+  hoverBgColor: string,
+  initHeight: number,
+  addHeight: number,
+) => css`
   cursor: pointer;
   text-decoration: none;
   padding: 10px 30px;
 
-  background-color: #fe0096;
-  color: #fff;
+  background-color: ${bgColor};
+  color: ${color};
   font-size: 1.25rem;
   font-weight: 700;
   letter-spacing: 1px;
 
-  border: 1px solid #e40087;
+  border: 2px solid ${hoverBgColor};
   border-radius: 4px;
-  box-shadow: 0 ${initHeight}px 0 #e40087;
+  box-shadow: 0 ${initHeight}px 0 ${hoverBgColor};
   transition: background-color 150ms linear, transform 150ms ease, box-shadow 150ms ease;
 
   &:hover {
     transform: translateY(-${addHeight}px);
-    box-shadow: 0 ${initHeight + addHeight}px 0 #e40087;
+    box-shadow: 0 ${initHeight + addHeight}px 0 ${hoverBgColor};
   }
 
   &:active {
-    background-color: #e40087;
+    background-color: ${hoverBgColor};
     transform: translateY(0);
     box-shadow: 0 ${initHeight}px 0 #e40087;
   }
