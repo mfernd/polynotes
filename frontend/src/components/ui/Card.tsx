8a7095364e8c6f5ac5030889f93d39baae4846f4
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-03-31 17:26:03 +0200

    feat(ui): rabbit animation

--- frontend/src/components/ui/Card.tsx
+++ frontend/src/components/ui/Card.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from 'react';
 import { css, SerializedStyles } from '@emotion/react';
 import logo from '@assets/images/logo.png';
+import { NavLink } from 'react-router-dom';
 
 type CardProps = {
   children?: ReactNode;
@@ -10,18 +11,21 @@ type CardProps = {
 };
 
 export const Card = (props: CardProps) => {
-  const buildedCss = css`
-    ${cardCss};
-    ${props.showLogo ? logoCss : ''};
-  `;
-
   return (
-    <article css={buildedCss}>
-      {props.title && <h1 css={cardTitleCss}>{props.title}</h1>}
-      <div css={props.cardContentCss}>
-        {props.children}
-      </div>
-    </article>
+    <div css={css`position: relative;`}>
+      {props.showLogo
+        ? (
+          <NavLink to={'/'} css={logoCss} title={"Revenir à l'accueil"}>
+            <img src={logo} alt={'revenir à l\'accueil'}/>
+          </NavLink>)
+        : null}
+      <article css={cardCss}>
+        {props.title && <h1 css={cardTitleCss}>{props.title}</h1>}
+        <div css={props.cardContentCss}>
+          {props.children}
+        </div>
+      </article>
+    </div>
   );
 };
 
@@ -37,21 +41,19 @@ const cardCss = css`
 `;
 
 const logoCss = css`
-  &::before {
-    content: "";
-    position: absolute;
-    top: -160px;
-    left: -120px;
-    transform: rotate(-20deg);
+  position: absolute;
+  top: -160px;
+  left: -120px;
+  transform: rotate(-20deg);
+  filter: drop-shadow(0 2px 0 rgb(0 0 0 / 0.1)) drop-shadow(2px 2px 0 rgb(0 0 0 / 0.1));
 
-    height: auto;
-    width: 225px;
-    aspect-ratio: 1;
+  height: auto;
+  width: 225px;
+  aspect-ratio: 1;
 
-    background-image: url("${logo}");
-    background-size: 100%;
-    filter: drop-shadow(0 2px 0 rgb(0 0 0 / 0.1)) drop-shadow(2px 2px 0 rgb(0 0 0 / 0.1));
-    z-index: -1;
+  :hover {
+    transition: transform 100ms ease-in-out;
+    transform: rotate(-20deg) scale(1.1);
   }
 `;
 
