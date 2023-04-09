31c169f6213ce03f2a655f78c39f105a70160af8
Lapsus Ordonné <22498591+lapsus-ord@users.noreply.github.com>
2023-04-09 18:18:14 +0200

    deploy(frontend): dockerfile (nginx)

---
+++ frontend/src/components/editor/blocks/ImageBlock.tsx
@@ -0,0 +1,18 @@
+import { DefaultBlockProps } from '@/typings/editor.type';
+import { css } from '@emotion/react';
+
+export const ImageBlock = (props: DefaultBlockProps) => {
+  return (
+    <div css={imageBlockCss}>
+      {props.node.settingsOpen
+        ? <span>coucou</span>
+        : <img src={props.node.data} alt={`image of block ${props.node.uuid}`}/>}
+    </div>
+  );
+};
+
+const imageBlockCss = css`
+  width: 100%;
+  border-radius: 1rem;
+  background-color: rgba(0 0 0 / 20%);
+`;
